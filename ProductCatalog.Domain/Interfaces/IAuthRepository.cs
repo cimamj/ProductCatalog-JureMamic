e018b0b52772dc5017c38be05d685af4f252cbfa
cimamj <jmamic02@fesb.hr>
2026-07-02 16:42:23 +0200

    Add auth handler, IJwtService interface, IAuthInterface in domain as User entity

---
+++ ProductCatalog.Domain/Interfaces/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Domain.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<string?> GetTokenAsync(string username, string password);
+        Task<User?> GetCurrentUserAsync(string token);
+    }
+}
